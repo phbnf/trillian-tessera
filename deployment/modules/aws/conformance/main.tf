a3de853b96d996c9390e12df44252d39e056ab0e
Philippe Boneff <phb@google.com>
2024-11-29 14:22:18 +0000

    one task two containers

--- deployment/modules/aws/conformance/main.tf
+++ deployment/modules/aws/conformance/main.tf
@@ -127,7 +127,7 @@ resource "aws_ecs_task_definition" "hammer" {
             "command": [
                 "--log_public_key=phboneff-dev-ci-conformance+3f5267c1+AatjnH2pMn2wRamVV1hywQI/+lHsV8ftCBroiCWyOUWQ",
                 "--log_url=https://phboneff-dev-ci-conformance-bucket.s3.us-east-1.amazonaws.com",
-                "--write_log_url=http://172.31.66.80:2024",
+                "--write_log_url=http://127.0.0.1:2024",
                 "-v=3",
                 "--show_ui=false",
                 "--logtostderr",
@@ -157,10 +157,110 @@ resource "aws_ecs_task_definition" "hammer" {
   }
 }
 
+resource "aws_ecs_task_definition" "conformance-all" {
+  family                   = "conformance"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  # TODO(phboneff): change this
+  execution_role_arn       = "arn:aws:iam::869935063533:role/ecsTaskExecutionRole"
+  container_definitions = jsonencode([
+    {
+       "name": "${local.name}-conformance",
+       "image": "869935063533.dkr.ecr.us-east-1.amazonaws.com/transparency-dev/phbtest-trillian-tessera",
+       "cpu": 0,
+       "portMappings": [
+           {
+               "name": "conformance-2024-tcp",
+               "containerPort": 2024,
+               "hostPort": 2024,
+               "protocol": "tcp",
+               "appProtocol": "http"
+           }
+       ],
+       "essential": true,
+       "command": [
+           "--signer",
+           "PRIVATE+KEY+phboneff-dev-ci-conformance+3f5267c1+AbNthDVVl8SUoHuxMtSxGjHXi5R+CivYtyO7M2TPVSi6",
+           "--bucket",
+           "phboneff-dev-ci-conformance-bucket",
+           "--db_user",
+           "root",
+           "--db_password",
+           "password",
+           "--db_name",
+           "tessera",
+           "--db_host",
+           "phboneff-dev-ci-conformance-writer-0.cbw8guwmo1hn.us-east-1.rds.amazonaws.com",
+           "-v",
+           "2"
+       ],
+        # TODO(phboneff): reenabel this
+        # "logConfiguration": {
+        #     "logDriver": "awslogs",
+        #     "options": {
+        #         "awslogs-group": "/ecs/conformance",
+        #         "mode": "non-blocking",
+        #         "awslogs-create-group": "true",
+        #         "max-buffer-size": "25m",
+        #         "awslogs-region": "us-east-1",
+        #         "awslogs-stream-prefix": "ecs"
+        #     },
+        # },
+    },
+    {
+       "name": "${local.name}-hammer",
+       "image": "869935063533.dkr.ecr.us-east-1.amazonaws.com/transparency-dev/phbtest-hammer:latest",
+       "cpu": 0,
+       "portMappings": [
+           {
+               "name": "hammer-80-tcp",
+               "containerPort": 80,
+               "hostPort": 80,
+               "protocol": "tcp",
+               "appProtocol": "http"
+           }
+       ],
+       "essential": true,
+       "command": [
+           "--log_public_key=phboneff-dev-ci-conformance+3f5267c1+AatjnH2pMn2wRamVV1hywQI/+lHsV8ftCBroiCWyOUWQ",
+           "--log_url=https://phboneff-dev-ci-conformance-bucket.s3.us-east-1.amazonaws.com",
+           "--write_log_url=http://127.0.0.1:2024",
+           "-v=3",
+           "--show_ui=false",
+           "--logtostderr",
+           "--num_writers=1100",
+           "--max_write_ops=1500",
+           "--leaf_min_size=1024",
+           "--leaf_write_goal=50000"
+          ],
+            # TODO(phboneff): re-enable
+            #"logConfiguration": {
+            #    "logDriver": "awslogs",
+            #    "options": {
+            #        "awslogs-group": "/ecs/hammer",
+            #        "mode": "non-blocking",
+            #        "awslogs-create-group": "true",
+            #        "max-buffer-size": "25m",
+            #        "awslogs-region": "us-east-1",
+            #        "awslogs-stream-prefix": "ecs"
+            #    },
+            #},
+    }
+  ])
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
+
+
 resource "aws_ecs_service" "conformance_service" {
-  name            = "${local.name}-conformance"
+  name            = "${local.name}-conformance-all"
   cluster         = aws_ecs_cluster.conformance-test.arn
-  task_definition = aws_ecs_task_definition.conformance.arn
+  task_definition = aws_ecs_task_definition.conformance-all.arn
   desired_count   = 3
   network_configuration {
     subnets = [aws_default_subnet.subnet.id]
