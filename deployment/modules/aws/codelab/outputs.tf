468129f7d707348bcdccf03caf6b600a8cf755ad
Philippe Boneff <phb@google.com>
2024-12-01 23:17:29 +0000

    better outputs

--- deployment/modules/aws/codelab/outputs.tf
+++ deployment/modules/aws/codelab/outputs.tf
@@ -1,9 +1,9 @@
 output "log_bucket" {
   description = "Log S3 bucket endpoint"
-  value       = module.aws_s3_bucket.log_bucket.bucket_regional_domain_name
+  value       = module.storage.log_bucket.bucket_regional_domain_name
 }
 
 output "log_rds_db" {
   description = "Log RDS database endpoint"
-  value       = module.aws_rds_cluster.log_rds.endpoint
+  value       = module.storage.log_rds_db.endpoint
 }
